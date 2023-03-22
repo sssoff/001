9f2357a7e7989b9365007c70219cff97b54dfc2c
sssoff <123302845+sssoff@users.noreply.github.com>
2023-03-22 16:37:14 +0300

    Add files via upload

---
+++ Dom 16.7.1/scr_2.py
@@ -0,0 +1,7 @@
+from scr_1 import Cat
+cat_1 = Cat("Барон", "мальчик", 2)
+cat_2 = Cat("Сэм", "мальчик", 2)
+
+print(f"Имя: {cat_1.name}, Пол: {cat_1.gender}, Возраст: {cat_1.age}")
+print(f"Имя: {cat_2.name}, Пол: {cat_2.gender}, Возраст: {cat_2.age}")
+
